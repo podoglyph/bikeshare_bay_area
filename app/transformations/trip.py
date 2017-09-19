8dc7a192a160455896a49adf6c0499fcd85e1d8a
Padraic O'Neill <pconeill@gmail.com [core]>
2017-09-19 12:30:34 -0600

    merges station

---
+++ app/transformations/trip.py
@@ -0,0 +1,41 @@
+import os
+import pandas as pd
+import datetime as datetime
+
+DATA_PATH = "data"
+
+def prep_trip_data():
+    trip_path = os.path.join("trip_fixture.csv")
+    return pd.read_csv(trip_path)
+
+df_trip = prep_trip_data()
+
+def transform_df_trip(data):
+    # data.dropna(axis=0, how='any', inplace=True)
+    data['duration'] /= 60
+    data.start_date = pd.to_datetime(data.start_date, format='%m/%d/%Y %H:%M')
+    data['date'] = data.start_date.dt.date
+    # data.drop(["start_station_name", "end_date", "bike_id", "subscription_type", "start_station_id", "end_station_name", "end_station_id"], 1, inplace=True)
+
+    dates = {}
+    for d in data.date:
+        if d not in dates:
+            dates[d] = 1
+        else:
+            dates[d] += 1
+
+    dates
+
+    data.drop("start_date", 1, inplace=True)
+    df = pd.DataFrame.from_dict(dates, orient = "index")
+    df['date'] = df.index
+    df['trips'] = df.iloc[:,0]
+    trips_train = df.iloc[:,1:3]
+    trips_train.reset_index(drop = True, inplace = True)
+    trips_train = trips_train.sort_values('date')
+    trips_train.reset_index(drop=True, inplace=True)
+    return trips_train
+
+transformed_trip = transform_df_trip(df_trip)
+trips_per_day = {}
+trips_per_day = transformed_trip.to_dict(orient="list")
