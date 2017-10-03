f75a40a1a74b435a9a4b2bfd196b7f27323e9f88
Padraic O'Neill <pconeill@gmail.com [core]>
2017-10-02 23:59:23 -0600

    creates methods for each weather transformation

--- app/transformations/weather.py
+++ app/transformations/weather.py
@@ -6,31 +6,32 @@ def run(city):
     weather = load_weather_data()
     weather = norm_precipitation_inches(weather)
     weather = norm_date(weather)
-    weather = select_city(city)
+    weather = select_city(weather, city)
     events = norm_events(weather.events)
     weather = merge_events(weather, events)
 
     return drop_labels(weather)
 
 
-#private
 def load_weather_data():
     weather_path = os.path.join("data/weather.csv")
     return pd.read_csv(weather_path)
 
 def norm_precipitation_inches(weather):
+    weather.precipitation_inches = pd.to_numeric(weather.precipitation_inches, errors = 'coerce')
     weather.loc[weather.precipitation_inches.isnull(),
         'precipitation_inches'] = weather[weather.precipitation_inches.notnull()].precipitation_inches.median()
-        
-    return weather.precipitation_inches = pd.to_numeric(weather.precipitation_inches, errors = 'coerce')
+
+    return weather
 
 def norm_date(weather):
-    return weather.date = pd.to_datetime(weather.date, format='%m/%d/%Y')
+    weather.date = pd.to_datetime(weather.date, format='%m/%d/%Y')
+    return weather
 
-def select_city(city):
+def select_city(weather, city):
     if city == "San Francisco":
         city_zip = 94107
-    else city == "San Jose":
+    elif city == "San Jose":
         city_zip = 95113
 
     return weather[weather.zip_code == city_zip]
@@ -44,14 +45,10 @@ def merge_events(weather, events):
     return weather.merge(events, left_index=True, right_index=True)
 
 def drop_labels(weather):
-    weather.drop(['events'], axis=1)
-    weather = weather.drop("zip_code", 1)
+    weather.drop(['events', 'precipitation_inches', 'zip_code', 'max_gust_speed_mph'], axis=1, inplace=True)
     return weather
 
 
-
-
-
-weather_train.drop(["events", "precipitation_inches"], 1, inplace=True)
-
 weather = run("San Francisco")
+
+# weather.shape(733, 25)
